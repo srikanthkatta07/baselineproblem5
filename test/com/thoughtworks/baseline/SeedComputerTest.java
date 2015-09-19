f633e24023b0bac324b652401079e4f1fe27252d
srikantk <kaanth.katta@gmail.com>
2015-09-19 13:03:53 +0530

    should computes the output seed of the boat pattern by seed computer

--- test/com/thoughtworks/baseline/SeedComputerTest.java
+++ test/com/thoughtworks/baseline/SeedComputerTest.java
@@ -25,4 +25,32 @@ public class SeedComputerTest {
 
         assertEquals(seed, seedComputer.computeTheSeed());
     }
+
+    @Test
+    public void shouldComputesTheEachCellOfBoatPatternAndGeneratesTheOutputCells() {
+        ArrayList<Cell> seed = new ArrayList<Cell>();
+        Cell firstCell = new Cell(0, 0, "X");
+        Cell secondCell = new Cell(0, 1, "X");
+        Cell thirdCell = new Cell(0, 2, "-");
+        Cell fourthCell = new Cell(1, 0, "X");
+        Cell fifthCell = new Cell(1, 1, "-");
+        Cell sixthCell = new Cell(1, 2, "X");
+        Cell seventhCell = new Cell(2, 0, "-");
+        Cell eightCell = new Cell(2, 1, "X");
+        Cell ninthCell = new Cell(2, 2, "-");
+
+        seed.add(firstCell);
+        seed.add(secondCell);
+        seed.add(thirdCell);
+        seed.add(fourthCell);
+        seed.add(fifthCell);
+        seed.add(sixthCell);
+        seed.add(seventhCell);
+        seed.add(eightCell);
+        seed.add(ninthCell);
+
+        SeedComputer seedComputer = new SeedComputer(seed);
+
+        assertEquals(seed, seedComputer.computeTheSeed());
+    }
 }
