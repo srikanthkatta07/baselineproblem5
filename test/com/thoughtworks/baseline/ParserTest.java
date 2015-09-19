18c36d34604e97eb40606055613f74554846503f
srikantk <kaanth.katta@gmail.com>
2015-09-19 13:09:15 +0530

    should generate output cell objects correponding to input block pattern from generator by the parser

--- test/com/thoughtworks/baseline/ParserTest.java
+++ test/com/thoughtworks/baseline/ParserTest.java
@@ -10,7 +10,7 @@ public class ParserTest {
 
     @Test
     public void shouldTakeTheInputBlockPatternFromTheGeneratorAndReturnsTheListOfCells() {
-        Parser parser = new Parser("XXXX",2,2);
+        Parser parser = new Parser("XXXX", 2, 2);
         ArrayList<Cell> seed = new ArrayList<Cell>();
         Cell firstCell = new Cell(0, 0, "X");
         Cell secondCell = new Cell(0, 1, "X");
@@ -21,6 +21,33 @@ public class ParserTest {
         seed.add(secondCell);
         seed.add(thirdCell);
         seed.add(fourthCell);
+
+        assertEquals(seed, parser.parseInputPattern());
+    }
+
+    @Test
+    public void shouldTakeTheInputBoatPatternFromTheGeneratorAndReturnsTheListOfCells() {
+        Parser parser = new Parser("XX-X-X-X-", 3, 3);
+        ArrayList<Cell> seed = new ArrayList<Cell>();
+        Cell firstCell = new Cell(0, 0, "X");
+        Cell secondCell = new Cell(0, 1, "X");
+        Cell thirdCell = new Cell(0, 2, "-");
+        Cell fourthCell = new Cell(1, 0, "X");
+        Cell fifthCell = new Cell(1, 1, "-");
+        Cell sixthCell = new Cell(1, 2, "X");
+        Cell seventhCell = new Cell(2, 0, "-");
+        Cell eightCell = new Cell(2, 1, "X");
+        Cell ninthCell = new Cell(2, 2, "-");
+
+        seed.add(firstCell);
+        seed.add(secondCell);
+        seed.add(thirdCell);
+        seed.add(fourthCell);
+        seed.add(fifthCell);
+        seed.add(sixthCell);
+        seed.add(seventhCell);
+        seed.add(eightCell);
+        seed.add(ninthCell);
 
         assertEquals(seed, parser.parseInputPattern());
     }
